2bc8c8fea35393c61faaa6e51eac429cad601479
IvebbI <knurowskikarol12345@gmail.com>
2024-04-25 11:23:14 +0200

    Usunięto niepotrzebne pliki
    - usunięcie zbędnych plików

--- praca/ogloszenie_o_prace.php
+++
@@ -1,7 +0,0 @@
-<?php
-  session_start();
-  if(!isset($_SESSION['usermail'])){
-     header('location:/system_ogloszeniowy-Internetowe-/logowanie/login_form.php');
-  }
- 
-?>
--- uzytkownik/mojekonto.php
+++
@@ -1,8 +0,0 @@
-<?php
-  session_start();
-  if(!isset($_SESSION['id'])){
-     header('location:/system_ogloszeniowy-Internetowe-/logowanie/login_form.php');
-  }
- 
-
-?>
